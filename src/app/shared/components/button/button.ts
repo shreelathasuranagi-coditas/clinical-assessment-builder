8e61a9b8273e3be5ae8c428c81999f07cfc3884b
nandini.korukonda-coditas <nandini.korukonda@coditas.com>
2025-12-23 14:55:19 +0530

    Changed Shared Features

--- src/app/shared/components/button/button.ts
+++ src/app/shared/components/button/button.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './button.scss',
 })
 export class Button {
-
+    primary = input<boolean>(false);
+  secondary = input<boolean>(false);
+  disabled = input<boolean>(false);
 }

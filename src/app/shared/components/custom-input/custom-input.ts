f880f55202095b0c61252d86e04307d43c7e5492
nandini.korukonda-coditas <nandini.korukonda@coditas.com>
2025-12-23 14:37:30 +0530

    Shared Component Initial Setup

---
+++ src/app/shared/components/custom-input/custom-input.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-input',
+  imports: [],
+  templateUrl: './custom-input.html',
+  styleUrl: './custom-input.scss',
+})
+export class CustomInput {
+
+}
